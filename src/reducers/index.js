444ed6b8adcd266626fd2875eda870b3ba43173b
Sotiris Ioannou <sotiris@jsmate.net>
2019-05-20 08:43:38 +0200

    Created skeleton for redux.

---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from "redux";
+import listitems from './listitems';
+
+const rootReducer = combineReducers({listitems});
+
+export default rootReducer
---
+++ src/reducers/listitems.js
@@ -0,0 +1,32 @@
+// list items actions
+export const CREATE_ITEM = 'CREATE_ITEM';
+
+const initialState = {
+  itemId: 0,
+  items: []
+};
+
+const createItem = (state, itemId) => {
+  itemId += 1;
+  return {
+      ...state,
+      itemId: itemId,
+      items: [
+          ...state.items,
+          {
+              id: state.itemId += 1,
+              title: 'title',
+              content: 'content'
+          }
+      ]
+  }
+};
+
+export default function listitems(state = initialState, action) {
+    switch (action.type) {
+        case CREATE_ITEM:
+            return createItem(state, state.itemId);
+        default:
+            return state;
+    }
+}
