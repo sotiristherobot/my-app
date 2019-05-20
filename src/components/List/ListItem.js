457a281552f1114a4af87603334dee7e3169785d
Sotiris Ioannou <sotiris@jsmate.net>
2019-05-20 11:03:38 +0200

    Some small css improvements for listitems.

--- src/components/List/ListItem.js
+++ src/components/List/ListItem.js
@@ -1,16 +1,19 @@
 import React from "react";
+import './ListItem.css';
 
 export default function ListItem(props) {
   return (
-    <div>
+    <div className="listItemWrapper">
       <input
         name="title"
+        className="listItem"
         onChange={e => props.onTextFieldInputChange(e, props.id)}
         placeholder="title"
         value={props.title}
       />
       <input
         name="content"
+        className="listItem"
         onChange={e => props.onTextFieldInputChange(e, props.id)}
         placeholder="content"
         value={props.content}
