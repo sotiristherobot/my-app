e417f48e9a1aa3299277dd9259e062f0b0b21f51
Sotiris Ioannou <sotiris@jsmate.net>
2019-05-21 15:07:25 +0200

    Move header to a separate directory

---
+++ src/components/Header/Header.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { Box, Button } from "grommet";
+import { Add, Login } from "grommet-icons";
+
+export default React.forwardRef((props, ref) => {
+  return (
+    <header>
+      <Box
+        direction="row"
+        flex={true}
+        gap="small"
+        align="center"
+        justify="between"
+        margin="small"
+      >
+        <h1>{props.title}</h1>
+        <Box direction="row" gap="small">
+            {
+                //do not create the addItemButton if there is no handler for it.
+                props.onAddItemButtonClick && <Button
+                disabled={props.disabled}
+                ref={ref}
+                icon={<Add/>}
+                onClick={props.onAddItemButtonClick}
+                label={props.addButtonText}
+                />
+            }
+            {
+                //do not create the addItemButton if there is no handler for it.
+                props.onLoginButtonClick && <Button
+                    disabled={props.disabled}
+                    ref={ref}
+                    icon={<Login/>}
+                    onClick={props.onLoginButtonClick}
+                    label={props.loginButtonText}
+                />
+            }
+        </Box>
+      </Box>
+    </header>
+  );
+});
