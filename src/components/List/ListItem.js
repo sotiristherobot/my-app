901c23f3bb9b03114acf08a003661bfdfa759622
Sotiris Ioannou <sotiris@jsmate.net>
2019-05-23 09:22:25 +0200

    Save button for each item in the list.

--- src/components/List/ListItem.js
+++ src/components/List/ListItem.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from "react";
-import { Box, TextInput, Text } from "grommet";
+import { Box, TextInput, Text, Button } from "grommet";
+import { Save } from "grommet-icons/es6";
 
 export default function ListItem(props) {
   return (
@@ -11,27 +12,28 @@ export default function ListItem(props) {
       justify="evenly"
       margin="small"
     >
-      {
-        props.editMode?
-            <Fragment>
-              <TextInput
-                  name="title"
-                  placeholder="title"
-                  value={props.title}
-                  onChange={e => props.onTextFieldInputChange(e, props.id)}
-              />
-              <TextInput
-                name="content"
-                placeholder="content"
-                value={props.content}
-                onChange={e => props.onTextFieldInputChange(e, props.id)}
-              />
-            </Fragment>:
-            <Fragment>
-              <Text>{props.title}</Text>
-              <Text>{props.content}</Text>
-            </Fragment>
-      }
+      {props.editMode ? (
+        <Fragment>
+          <TextInput
+            name="title"
+            placeholder="title"
+            value={props.title}
+            onChange={e => props.onTextFieldInputChange(e, props.id)}
+          />
+          <TextInput
+            name="content"
+            placeholder="content"
+            value={props.content}
+            onChange={e => props.onTextFieldInputChange(e, props.id)}
+          />
+          <Button icon={<Save />} />
+        </Fragment>
+      ) : (
+        <Fragment>
+          <Text>{props.title}</Text>
+          <Text>{props.content}</Text>
+        </Fragment>
+      )}
     </Box>
   );
 }
